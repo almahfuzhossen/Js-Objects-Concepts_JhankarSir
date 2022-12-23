928575915b4ce95acdc73a9474dafcb617b34c41
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-23 22:52:56 +0600

    compare object

--- compare-object.js
+++ compare-object.js
@@ -16,3 +16,15 @@ console.log(JSON.stringify(second2));
 if(JSON.stringify(first2) === JSON.stringify(second2)){
     // console.log('objects are equal')
 }
+
+function compareObjects(obj1, onj2){
+    if (Object.keys(obj1) !== Object.keys(obj2).length){
+        return false;
+    }
+    for (const prop in obj1){
+        if ( obj1[prop]){
+            return false;
+        }
+    }
+    return true;
+}
