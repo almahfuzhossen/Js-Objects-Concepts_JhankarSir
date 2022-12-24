4dc0c8c4d14e82ee8034f63d8a2c26624ba9b77c
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-24 19:25:24 +0600

    bind method

---
+++ bind-method.js
@@ -0,0 +1,32 @@
+const kibria = {
+    id: 101,
+    money: 5000,
+    name: 'RJ Kibria',
+    treatDey: function(expense) {
+        this.money = this.money - expense;
+        console.log('hero5555', this);
+        return this.money;
+    }
+}
+
+const heroBalam = {
+    id : 102,
+    money : 6000,
+    name : 'hero balam'
+}
+
+
+const heroGolam = {
+    id : 103,
+    money : 8000,
+    name : 'hero golam'
+}
+
+kibria.treatDey(100);
+const heroTreatDey = kibria.treatDey.bind(heroBalam);
+heroTreatDey(500);
+heroTreatDey(300);
+heroTreatDey(500);
+kibria.treatDey(400);
+const normalTreatDey = kibria.treatDey.bind(normalGolam);
+normalTreatDey(2000);
